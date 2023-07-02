6c123863a4491d491403d53cffd2a8e8fee3ebea
Marie Kauth <marie.kauth@gmail.com>
2023-07-02 00:40:32 -0400

    Fixed the CSS. The game now works

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-using ConnectFour;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ConnectFour;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
